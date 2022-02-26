93b08b1802dd92502c0c30a0796a498c2fe181e8
VladKalayda <43132252+VladKalayda@users.noreply.github.com>
2022-02-26 16:11:00 +0200

    Update docker_entrypoint.sh
    Pass all args to the ripper

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-HOSTNAME=$1
-IP=$(dig +short "$HOSTNAME"| tail -n 1)
-python3 -u DRipper.py -s "$IP" -t 135 -p 443
+#HOSTNAME=$1
+#IP=$(dig +short "$HOSTNAME"| tail -n 1)
+python3 -u DRipper.py ${@}
